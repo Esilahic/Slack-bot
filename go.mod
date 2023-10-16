1e27d14d0f1aae0e78c10de786a53ccebf86d84d
Esilahic <silahicemil11@gmail.com>
2023-10-16 13:46:36 -0600

    testing stuff again

---
+++ go.mod
@@ -0,0 +1,3 @@
+module slackbot
+
+go 1.21.3
--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-  fmt.Println("test")
+	txt := []byte("this is a test\n")
+	err := os.WriteFile("test.txt", txt, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	f, err := os.Create("test2.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	d2 := []byte{1, 2, 3}
+	_, err = f.Write(d2)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
