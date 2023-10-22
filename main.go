c8ee566c13d88bea40250cd9847277e8b4ab094a
Esilahic <silahicemil11@gmail.com>
2023-10-22 16:36:16 -0600

    refresh

--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
-	f, err := os.Create("test2.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer f.Close()
 
-	d2 := []byte{115, 111, 109, 101, 10}
-	n2, err := f.Write(d2)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("wrote %d bytes\n", n2)
-
-	data, err := os.ReadFile("test2.txt")
-	if err != nil {
-		panic(err)
-	}
-	for _, v := range data {
-		fmt.Printf("wow %v\n", v)
-	}
 }
