5f200531f784f7387e7946a063fa55afab887b77
Esilahic <silahicemil11@gmail.com>
2023-10-22 19:54:13 -0600

    workin hard or hardly workin

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module slackbot
 
 go 1.21.3
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/slack-go/slack v0.12.3
+)
+
+require github.com/gorilla/websocket v1.5.0 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
-//imports gotdotenv and slack-go
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
 
 func main() {
 	//godotenv load
-
+	godotenv.Load(".env")
+	token := os.Getenv("SLACK_API_TOKEN")
+	channel := os.Getenv("CHANNEL_ID_TOKEN")
 	//slack api token and channel id
-
+	api := slack.New(token)
 	//slack-go -read docs and create bot
 
 }
