28b6d2f6590109d090afb536a165fd932f9a13a2
Esilahic <silahicemil11@gmail.com>
2023-10-30 22:43:21 -0600

    error stop fix later

--- socketmode/socketmode.go
+++ socketmode/socketmode.go
@@ -1,8 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
+)
+
+func HandleEventMessage(event slackevents.EventsAPIEvent, client *slack.Client) error {
+    switch event.Type {
+ 
+    case slackevents.CallbackEvent:
+ 
+        innerEvent := event.InnerEvent
+ 
+        switch evnt := innerEvent.Data.(type) {
+            err := HandleAppMentionEventToBot(evnt, client)
+            if err != nil {
+                return err
+            }
+        }
+    default:
+        return errors.New("unsupported event type")
+    }
+    return nil
+}
 
 func main() {
 	// TODO: add eventlistener for user commands in slack client
-	fmt.Println("this is a test!")
+	godotenv.Load("../.env")
+	token := os.Getenv("SLACK_API_TOKEN")
+	websocket := os.Getenv("SLACK_APP_TOKEN")
+
+	client := slack.New(token, slack.OptionAppLevelToken(websocket))
+
+	socketClient := socketmode.New(
+		client,
+		socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
+
+	go func(ctx context.Context, client *slack.Client, socketClient *socketmode.Client) {
+		for {
+			select {
+			case <-ctx.Done():
+				log.Println("Shutting down socketmode listener")
+				return
+			case event := <-socketClient.Events:
+
+				switch event.Type {
+
+				case socketmode.EventTypeEventsAPI:
+
+					eventsAPI, ok := event.Data.(slackevents.EventsAPIEvent)
+					if !ok {
+						log.Printf("Could not type cast the event to the EventsAPI: %v\n", event)
+						continue
+					}
+
+					socketClient.Ack(*event.Request)
+					err := HandleEventMessage(eventsAPI, client)
+                    if err != nil {
+                        log.Fatal(err)
+                    }
+				}
+			}
+		}
+	}(ctx, client, socketClient)
+
+	socketClient.Run()
 }
